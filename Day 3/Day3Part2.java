73d1202a442e2d2c4952de0114c274e6b7606f8a
Benjamin Loh <benjaminloh.2023@scis.smu.edu.sg>
2024-12-05 20:32:39 +0800

    Tried to fix solution, no clue why not working

--- Day 3/Day3Part2.java
+++ Day 3/Day3Part2.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.*;
 
 public class Day3Part2 {
     public static void main(String[] args) {
@@ -25,107 +26,61 @@ public class Day3Part2 {
         }
     }
 
-    public static int checkMult(String line) {
-        int total = 0;
-
-        // First part: Find the substring "mul("
-
-        String mulStr = "mul(";
-
-        int currentIndex = line.indexOf(mulStr);
-
-        if (currentIndex != -1) {
-
-            String firstNumStr = "";
-
-            currentIndex += mulStr.length();
-
-            char currentChar = line.charAt(currentIndex);
-
-            while (currentChar != ',') {
-                if (Character.isDigit(currentChar)) {
-                    firstNumStr += currentChar;
-                    currentIndex++;
-                    currentChar = line.charAt(currentIndex);
-                } else {
-                    firstNumStr = "";
-                    break;
-                }
-            }
-
-            if (firstNumStr != "") {
-                int firstNum = Integer.parseInt(firstNumStr);
-
-                // Current at ',', so need to add 1 more to the index
-                String secondNumStr = "";
-
-                currentIndex += 1;
-
-                currentChar = line.charAt(currentIndex);
-
-                while (currentChar != ')') {
-                    if (Character.isDigit(currentChar)) {
-                        secondNumStr += currentChar;
-                        currentIndex++;
-                        currentChar = line.charAt(currentIndex);
-                    } else {
-                        secondNumStr = "";
-                        break;
-                    }
-                }
-
-                if (secondNumStr != "") {
-                    int secondNum = Integer.parseInt(secondNumStr);
-
-                    total += firstNum * secondNum;
-                }
-
-                total += checkMult(line.substring(currentIndex));
-
-            } else {
-                total += checkMult(line.substring(currentIndex));
-            }
-        }
-        return total;
-    };
-
     public static int checkMult2(String line) {
+
+        String regex = "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)";
+        Pattern pattern = Pattern.compile(regex);
+        Pattern innerPattern = Pattern.compile("\\d+");
+        boolean mulEnabled = true;
         int total = 0;
 
-        String startStr = "do()";
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+            String match = matcher.group();
 
-        String endStr = "don't()";
+            if (match.startsWith("mul(")) {
+                if (mulEnabled) {
+                    // Matcher innerMatch = innerPattern.matcher(match);
+                    Matcher innerMatch = innerPattern.matcher(match);
 
-        int lowerBound = line.indexOf(startStr);
-        int upperBound = line.indexOf(endStr);
+                    int[] nums = { 0, 0 };
+                    int counter = 0;
 
-        // There is no substring called "don't()", meaning we execute all mul() in the whole line
-        if (upperBound == -1) {
-            total += checkMult(line);
+                    while (innerMatch.find()) {
+                        nums[counter] = Integer.parseInt(innerMatch.group());
+                        counter++;
+                    }
 
-        // Check position of "don't()" relative to "do()", whether it's before or after
-        } else {
-
-            // If "do()" comes after "don't()", we execute all muls until "don't()", then recursively check the substring after "do()" for more muls
-            if (lowerBound > upperBound) {
-
-                total += checkMult(line.substring(0, upperBound));
-                total += checkMult2(line.substring(lowerBound + startStr.length()));
-
-            // If "do()" comes before "don't()", we need to check more things
-            } else {
-
-                // If "do()"" doesn't exist at all, we only check for muls until the "don't()", and forget about the rest of the string 
-                if (lowerBound == -1) {
-                    total += checkMult(line.substring(0, upperBound));
-
-                // else, we ignore this "do()" and just make a substring so we can find the next "do()" 
-                } else {
-                    total += checkMult2(line.substring(lowerBound + startStr.length()));
+                    total += nums[0] * nums[1];
                 }
+            } else if (match.equals("do()")) {
+                mulEnabled = true;
+            } else if (match.equals("don't()")) {
+                mulEnabled = false;
             }
-        }
 
+            // if (match.equals("do()")) {
+            //     mulEnabled = true;
+            // } else if (match.equals("don't()")) {
+            //     mulEnabled = false;
+            // } else if (match.startsWith("mul(")) {
+            //     // Matcher innerMatch = innerPattern.matcher(match);
+            //     Matcher innerMatch = innerPattern.matcher(match);
+
+            //     int[] nums = { 0, 0 };
+            //     int counter = 0;
+
+            //     while (innerMatch.find()) {
+            //         nums[counter] = Integer.parseInt(innerMatch.group());
+            //         counter++;
+            //     }
+                
+            //     if (mulEnabled) {
+            //         total += nums[0] * nums[1];
+            //     }
+            // }
+        }
         return total;
+
     }
 }
